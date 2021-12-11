1d92851978ac9763aa7367b774ac7f9a25fea29e
VilliLiukuri <wrightfredfrik@gmail.com>
2021-12-11 20:40:41 +0200

    version 0.03

--- matrix_app.bat
+++ matrix_app.bat
@@ -1,4 +1,8 @@
 @echo off
 start chrome https://youtu.be/tU8yXfk7H-8?t=211
-start chrome https://youtu.be/_C2fSv49o8U?t=133
-start chrome https://youtu.be/PsBOBx7Ck6A?t=66
+start chrome https://youtu.be/TsqmH77Vg3M?t=152
+start chrome https://youtu.be/TsqmH77Vg3M?t=152
+start chrome https://youtu.be/TsqmH77Vg3M?t=152
+start chrome https://youtu.be/TsqmH77Vg3M?t=152
+start chrome https://youtu.be/TsqmH77Vg3M?t=152
+start chrome https://youtu.be/TsqmH77Vg3M?t=152
