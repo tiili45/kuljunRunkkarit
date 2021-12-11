1f14014b3c5c9acc682c13890c283a5a1e100df5
VilliLiukuri <wrightfredfrik@gmail.com>
2021-12-11 20:39:12 +0200

    version 0.02

--- matrix_app.bat
+++ matrix_app.bat
@@ -1,44 +1,4 @@
 @echo off
-start matrix.bat
-start images.jpg
-start matrix.bat
-start images.jpg
-start matrix.bat
-start images.jpg
-start matrix.bat
-start images.jpg
-start matrix.bat
-start images.jpg
-start matrix.bat
-start images.jpg
-start matrix.bat
-start images.jpg
-start matrix.bat
-start images.jpg
-start matrix.bat
-start images.jpg
-start matrix.bat
-start images.jpg
-start matrix.bat
-start images.jpg
-start matrix.bat
-start images.jpg
-start matrix.bat
-start images.jpg
-start matrix.bat
-start images.jpg
-start matrix.bat
-start images.jpg
-start matrix.bat
-start images.jpg
-start matrix.bat
-start images.jpg
-start matrix.bat
-start images.jpg
-start matrix.bat
-start images.jpg
-start matrix.bat
-start images.jpg
-timeout 10
-start matrix.bat
-start images.jpg
+start chrome https://youtu.be/tU8yXfk7H-8?t=211
+start chrome https://youtu.be/_C2fSv49o8U?t=133
+start chrome https://youtu.be/PsBOBx7Ck6A?t=66
