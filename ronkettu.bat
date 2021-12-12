c1c06d8b64232ec6c40e26981a7edeeba5af5ea8
Barbaari <vili.ae.holopainen@gmail.com>
2021-12-12 18:44:13 +0200

    ronviisli

---
+++ ronkettu.bat
@@ -0,0 +1,22 @@
+@echo off
+start firefox https://youtu.be/TsqmH77Vg3M?t=153
+timeout 5
+start firefox https://youtu.be/TsqmH77Vg3M?t=153
+timeout 5
+start firefox https://youtu.be/TsqmH77Vg3M?t=153
+timeout 5
+start firefox https://youtu.be/TsqmH77Vg3M?t=153
+timeout 5
+start firefox https://youtu.be/TsqmH77Vg3M?t=153
+timeout 5
+start firefox https://youtu.be/TsqmH77Vg3M?t=153
+timeout 5
+start firefox https://youtu.be/TsqmH77Vg3M?t=153
+timeout 5
+start firefox https://youtu.be/TsqmH77Vg3M?t=153
+timeout 5
+start firefox https://youtu.be/TsqmH77Vg3M?t=153
+timeout 5
+start firefox https://youtu.be/TsqmH77Vg3M?t=153
+timeout 5
+start firefox https://youtu.be/TsqmH77Vg3M?t=153
