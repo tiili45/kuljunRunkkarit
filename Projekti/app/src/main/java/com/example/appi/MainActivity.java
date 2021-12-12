4a03fd5cdeb7f16a9d23a549d280c631a9ccd2e2
tiili45 <juusokolari@gmail.com>
2021-12-12 17:37:52 +0200

    Kirjautumisikkunasta pääsee eteenpäin + kaupunkilistaus löytyy

--- Projekti/app/src/main/java/com/example/appi/MainActivity.java
+++ Projekti/app/src/main/java/com/example/appi/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.appi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView kaupunkiLista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.kirjautuminen);
+    }
+
+    /**
+     * @param view kirjaudu napin toiminnot
+     */
+    public void painaKirjaudu(View view) {
+        setContentView(R.layout.paaikkuna);
+        kaupunkiLista = findViewById(R.id.kaupunkiLista);
+        ArrayList<String> kaupungit = new ArrayList<String>();
+        kaupungit.add("Jyväskylä");
+        kaupungit.add("Tampere");
+        kaupungit.add("Helsinki");
+        kaupungit.add("Jyväskylä");
+        kaupungit.add("Tampere");
+        kaupungit.add("Helsinki");
+        kaupungit.add("Jyväskylä");
+        kaupungit.add("Tampere");
+        kaupungit.add("Helsinki");
+        ArrayAdapter<String> kaupunkiadapteri = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, kaupungit);
+        kaupunkiLista.setAdapter(kaupunkiadapteri);
     }
 }
